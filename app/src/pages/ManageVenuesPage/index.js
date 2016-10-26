842dace9e7073fd1af593a2b611ef175b76bda5d
Oscar Mendoza <oscar@smartwave.ph>
2016-10-26 13:54:09 +0800

    Correct Organisation ID

--- app/src/pages/ManageVenuesPage/index.js
+++ app/src/pages/ManageVenuesPage/index.js
@@ -18,180 +18,160 @@ import Header from 'grommet/components/Header';
 import Section from 'grommet/components/Section';
 import Paragraph from 'grommet/components/Paragraph';
 
-    /*const EVENTS = [ // GET all events
-    { value: '001', label: 'Girls Just Wanna Have Fun' }, // value = event.id // label = event.name?
-    { value: '002', label: 'Kate Mess' },
-    { value: '003', label: 'Game On'},
-    { value: '004', label: 'Overtime'}
-    ];*/
 
-    const organisationId = '57f3a273f760e4f8ad97eec5';
+const organisationId = '5800471acb97300011c68cf7';
 
-    class ManageVenuesPage extends Component {
-      constructor() {
-        super();
-        // this.handleMobile = this.handleMobile.bind(this);
-        // this.handleCalendar = this.handleCalendar.bind(this);
-        this.onEventChange = this.onEventChange.bind(this);
-        this.closeSetup = this.closeSetup.bind(this);
-        // this.onDrop = this.onDrop.bind(this);
-        // this.onRemoveImage = this.onRemoveImage.bind(this);
-        // this.setName = this.setName.bind(this);
-        // this.setDescription = this.setDescription.bind(this);
-        // this.setAddress = this.setAddress.bind(this);
-        this.submitCreate = this.submitCreate.bind(this);
+class ManageVenuesPage extends Component {
+  constructor() {
+    super();
+    // this.handleMobile = this.handleMobile.bind(this);
+    // this.handleCalendar = this.handleCalendar.bind(this);
+    this.onEventChange = this.onEventChange.bind(this);
+    this.closeSetup = this.closeSetup.bind(this);
+    // this.onDrop = this.onDrop.bind(this);
+    // this.onRemoveImage = this.onRemoveImage.bind(this);
+    // this.setName = this.setName.bind(this);
+    this.setDescription = this.setDescription.bind(this);
+    // this.setAddress = this.setAddress.bind(this);
+    this.submitCreate = this.submitCreate.bind(this);
 
-        const organisationId = '57f3a273f760e4f8ad97eec5';
+    const organisationId = '5800471acb97300011c68cf7';
 
-        this.state = {
-          isMobile: false,
-          image: [],
-          venueId: null, // id mock test
-          name: '',
-          description: '',
-          address: '',
-          confirm: false
-          //events: EVENTS,
-          //value: []
-        };
-      }
+    this.state = {
+      isMobile: false,
+      image: [],
+      venueId: null,
+      name: '',
+      description: '',
+      address: '',
+      confirm: false
+    };
+  }
 
-      componentWillMount(){
+  componentWillMount(){
 
-        let paramsVenueId = this.props.params.venueId
-
-        // console.log(paramsVenueId);
-
-        if (paramsVenueId != null) {
-
-          // Get Venue with Organisation Id and Venue Id 
-          PartyBot.venues.getWithOriganisationIdAndVenueId(organisationId, paramsVenueId, function(err, response, body) {
-
-            console.log(response.statusCode);
-            console.log(body);
-            console.log(err);
-
-            if(!err && response.statusCode == 200) {
-              this.setState({
-                name: body.name,
-                description: body.description,
-                venueId: paramsVenueId
-              })
-            }
-          }.bind(this));
+    let paramsVenueId = this.props.params.venueId
+    if (paramsVenueId != null) {
+      // Get Venue with Organisation Id and Venue Id 
+      PartyBot.venues.getWithOriganisationIdAndVenueId(organisationId, paramsVenueId, function(err, response, body) {
+        console.log(response.statusCode);
+        console.log(body);
+        console.log(err);
+        if(!err && response.statusCode == 200) {
+          this.setState({
+            name: body.name,
+            description: body.description,
+            venueId: paramsVenueId
+          })
         }
-      }
+      }.bind(this));
+    }
+  }
 
-      componentDidMount() {
-        if (typeof window !== 'undefined') {
-          window.addEventListener('resize', this.handleMobile.bind(this));
-        }   
+  componentDidMount() {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', this.handleMobile.bind(this));
+    }   
+  }
+  componentWillUnmount() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.handleMobile.bind(this));
+    }
+  }
+  handleMobile() {
+    const isMobile = window.innerWidth <= 768;
+    this.setState({
+      isMobile,
+    });
+  }
+  onEventChange(value) {
+    console.log('You\'ve selected:', value);
+    this.setState({ value });
+  }
+  handleCalendar(event) {
+    this.setState({calendar: event.target.value});
+  }
+  onDrop(file) {
+    this.setState({
+     image: file
+   });
+    console.log(file[0]);
+  }
+  closeSetup(){
+    this.setState({
+     confirm: false
+   });
+    this.context.router.push('/venues');
+  }
+  onRemoveImage() {
+    this.setState({
+      image: []
+    });
+  }
+  setName(event) {
+    this.setState({name: event.target.value});
+  }
+  setDescription(event) {
+    this.setState({ description: event.target.value });
+  }
+  setAddress(event) {
+    this.setState({
+      address: event.target.value
+    });
+  }
+  submitCreate() {
+
+    var objState = this.state;
+    var createParams = {
+      organisationId: organisationId,
+      name: objState.name,
+      description: objState.description,
+      location: {
+        address: objState.address
       }
-      componentWillUnmount() {
-        if (typeof window !== 'undefined') {
-          window.removeEventListener('resize', this.handleMobile.bind(this));
-        }
-      }
-      handleMobile() {
-        const isMobile = window.innerWidth <= 768;
+    };
+
+    console.log(createParams);
+
+    PartyBot.venues.create(createParams, function(err, response, body) {
+
+      console.log(createParams);
+
+      console.log(response.statusCode);
+      console.log(body);
+
+      if(response.statusCode == 200) {
+        console.log('Venue Created.');
         this.setState({
-          isMobile,
+          confirm: true
         });
       }
-      onEventChange(value) {
-        console.log('You\'ve selected:', value);
-        this.setState({ value });
-      }
-      handleCalendar(event) {
-        this.setState({calendar: event.target.value});
-      }
-      onDrop(file) {
-        this.setState({
-         image: file
-       });
-        console.log(file[0]);
-      }
-      closeSetup(){
-        this.setState({
-         confirm: false
-       });
-        this.context.router.push('/venues');
-      }
-      onRemoveImage() {
-        this.setState({
-          image: []
-        });
-      }
-      setName(event) {
-        this.setState({name: event.target.value});
-      }
-      setDescription(event) {
-        this.setState({ description: event.target.value });
-      }
-      setAddress(event) {
-        this.setState({
-          address: event.target.value
-        });
-      }
-      submitCreate() {
-
-        var objState = this.state;
-        var createParams = {
-          organisationId: organisationId,
-          name: objState.name,
-          // description: objState.description,
-          location: {
-            address: objState.address
-          }
-          // can't upload for now
-          // image: objState.image[0].preview
-        };
-
-        console.log(createParams);
-        // return;
-
-        PartyBot.venues.create(createParams, function(err, response, body) {
-
-          console.log(createParams);
-
-          console.log(response.statusCode);
-          console.log(body);
-
-          if(response.statusCode == 200) {
-            console.log('Venue Created.');
-            this.setState({
-              confirm: true
-            });
-          }
-        }.bind(this));
-      }
-      render() {
-        const {
-          router,
-        } = this.context;
-        const {
-          isMobile,
-        } = this.state;
-        const {
-          image,
-        } = this.state;
-
-    // console.log(image);
-
+    }.bind(this));
+  }
+  render() {
+    const {
+      router,
+    } = this.context;
+    const {
+      isMobile,
+    } = this.state;
+    const {
+      image,
+    } = this.state;
     return (
       <div className={styles.container}>
       <link rel="stylesheet" href="https://unpkg.com/react-select/dist/react-select.css" />
       {this.state.confirm !== false ? 
-      <Layer align="center">
+        <Layer align="center">
         <Header>
-            Venue successfully created.
+        Venue successfully created.
         </Header>
         <Section>
-          <Button label="Close" onClick={this.closeSetup} plain={true} icon={<CloseIcon />}/>
+        <Button label="Close" onClick={this.closeSetup} plain={true} icon={<CloseIcon />}/>
         </Section>
-      </Layer>
-      :
-      null
+        </Layer>
+        :
+        null
       }
       <Box pad={{ vertical: 'medium' }}>
       {this.state.venueId !== null ? 
@@ -211,81 +191,47 @@ import Paragraph from 'grommet/components/Paragraph';
       <FormField label="Name" htmlFor="venueName">
       <input id="venueName" type="text" onChange={this.setName.bind(this)} value={this.state.name} />
       </FormField>
-      {/*
       <FormField label="Description" htmlFor="venueDescription">
       <input id="venueDescription" type="text" onChange={this.setDescription} value={this.state.description} />
       </FormField>
-    */}
-    <FormField label="Address" htmlFor="venueAddress">
-    <input id="venueAddress" type="text" onChange={this.setAddress.bind(this)} value={this.state.address}/>
-    </FormField>
-      {/* 
-          <FormField label="Longitude" htmlFor="venueLon">
-          <input id="venueLon" type="number" />
-          </FormField>
-          <FormField label="Latitude" htmlFor="venueLat">
-          <input id="venueLat" type="number" />
-          </FormField>
-        */}
-      {/*
-      <Box separator="all">
-      <FormField label="Events" htmlFor="venueEvent"/>
-      <Select 
-          name="eventVenue"
-          options={this.state.events}
-          value={this.state.value}
-          onChange={this.onEventChange} 
-          multi={true}
-      />
-      </Box>
-      <FormField label="Starts At" htmlFor="venueStarts">
-      <DateTime id="venueStarts" format="M/D/YYYY" onChange={this.handleCalendar} value={this.state.calendar} />
+      <FormField label="Address" htmlFor="venueAddress">
+      <input id="venueAddress" type="text" onChange={this.setAddress.bind(this)} value={this.state.address}/>
       </FormField>
-      <FormField label="Ends At" htmlFor="venueEnds">
-      <DateTime id="venueEnds" format="M/D/YYYY" onChange={this.handleCalendar} value={this.state.calendar} />
-    </FormField> 
-  */}
-  <FormField label="Image">
-  {image.length > 0 ? 
-    <Box align="center" justify="center">
-  {/* commented out */}
-    {
-          /*
-          <div>{image.map((file) => <img src={file.preview} /> )}</div>
-          */
-        }
-      {/* commented out */}
-      <div> 
-      <img src={image[0].preview} width="200" />
-      </div>
-      <Box>
-      <Button label="Cancel" onClick={this.onRemoveImage.bind(this)} plain={true} icon={<CloseIcon />}/>
-      </Box>
-      </Box> :
-      <Box align="center" justify="center">
-      <Dropzone multiple={false} ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop.bind(this)} accept='image/*'>
-      Drop image here or click to select image to upload. 
-      </Dropzone>
-      </Box>
-    }
-    </FormField>
-    </fieldset>
-    </FormFields>
-    <Footer pad={{"vertical": "medium"}}>
-    {this.state.venueId !== null ? 
-     <Heading align="center">
-     <Button label="Save Changes" primary={true} onClick={this.submitSave.bind(this)} />
-     </Heading>
-     : 
-     <Heading align="center">
-     <Button label="Create Venue" primary={true} onClick={this.submitCreate.bind(this)} />
-     </Heading>
-   }
-   </Footer>
-   </Form>
-   </Box>
-   </div>
-   );
+      
+      <FormField label="Image">
+      {image.length > 0 ? 
+        <Box align="center" justify="center"> 
+        <div> 
+        <img src={image[0].preview} width="200" />
+        </div>
+        <Box>
+        <Button label="Cancel" onClick={this.onRemoveImage.bind(this)} plain={true} icon={<CloseIcon />}/>
+        </Box>
+        </Box> :
+        <Box align="center" justify="center">
+        <Dropzone multiple={false} ref={(node) => { this.dropzone = node; }} onDrop={this.onDrop.bind(this)} accept='image/*'>
+        Drop image here or click to select image to upload. 
+        </Dropzone>
+        </Box>
+      }
+      </FormField>
+      </fieldset>
+      </FormFields>
+      <Footer pad={{"vertical": "medium"}}>
+      {this.state.venueId !== null ? 
+       <Heading align="center">
+       <Button label="Save Changes" primary={true} onClick={this.submitSave.bind(this)} />
+       </Heading>
+       : 
+       <Heading align="center">
+       <Button label="Create Venue" primary={true} onClick={this.submitCreate.bind(this)} />
+       </Heading>
+     }
+     </Footer>
+     </Form>
+     </Box>
+     </div>
+     );
   }
 }
 
