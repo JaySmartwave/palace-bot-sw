f92ffebdc70c8440629ee4db7f2eeb2a454e6579
JaySmartwave <jay@smartwave.ph>
2016-10-28 00:13:31 +0800

    NOT DONE YET update guestlist UI (added checkbox)

--- app/src/pages/GuestListPage/index.js
+++ app/src/pages/GuestListPage/index.js
@@ -9,26 +9,42 @@ import Button from 'grommet/components/Button';
 import Box from 'grommet/components/Box';
 import Menu from 'grommet/components/Menu';
 import Image from 'grommet/components/Image';
+import CheckBox from 'grommet/components/CheckBox';
 import CheckmarkIcon from 'grommet/components/icons/base/Checkmark';
 import Table from 'grommet/components/Table';
 import TableRow from 'grommet/components/TableRow';
 import Search from 'grommet/components/Search';
 import DocumentCsvIcon from 'grommet/components/icons/base/DocumentCsv';
+import ActionsIcon from 'grommet/components/icons/base/Actions';
 
 const FILTER = [ 
     { value: '001', label: 'Approved' }, 
     { value: '002', label: 'Pending' },
+  ];
+      
+const EXAMPLE = [ //test 
+    { id: '001', name: 'Oscar', promoter: 'Jolibee', status: 'Pending'}, 
+    { id: '002', name: 'BJ', promoter: 'KFC', status: 'Pending'},
+    { id: '003', name: 'Jay', promoter: 'Jolibee', status: 'Pending'},
+    { id: '004', name: 'David', promoter: 'KFC', status: 'Approved'}, 
+    { id: '005', name: 'Barbo', promoter: 'Jolibee', status: 'Pending'},
+    { id: '006', name: 'Mendoza', promoter: 'KFC', status: 'Approved'},
   ];
 
 class GuestListPage extends Component {
   constructor() {
     super();
     this.handleMobile = this.handleMobile.bind(this);
+    this.selectAll = this.selectAll.bind(this);
+    this.checkToggle = this.checkToggle.bind(this);
     this.state = {
       isMobile: false,
+      selectedGuest: [],
       guestList: [],
       filter: FILTER,
-      activeFilter: '' 
+      example: EXAMPLE, //test
+      activeFilter: '', 
+      isAllSelected: false
      };
 
   }
@@ -72,8 +88,24 @@ class GuestListPage extends Component {
     });
   }
   testFunc() {
-  	console.log('test');
+  	console.log(this.state.selectedGuest);
   }
+
+  selectAll(){
+    var active = !this.state.isAllSelected;
+    this.setState({
+      isAllSelected: active,
+    });
+    // TO DO:
+    //add/remove id to/from [selectedGuest]
+  }
+
+  checkToggle(){
+      console.log(this.prop.id);
+      // TO DO:
+      // add/remove id to/from [selectedGuest]
+  }
+
   render() {
     const { router, } = this.context;
     const { isMobile, } = this.state;
@@ -102,15 +134,36 @@ class GuestListPage extends Component {
       <Table selectable={false}>
       <thead>
       <tr>
+      <th> 
+        <Box align="center">
+          <CheckBox name="label" onChange={this.selectAll} />
+        </Box>
+      </th>
       <th> Name </th>
       <th> Promoter</th>
       <th> Status </th>
-      <th> </th>
+      <th> 
+        <Box align="center">
+        {this.state.selectedGuest.length > 1 ?
+          <Menu icon={<ActionsIcon />} label="Actions">
+            <Anchor>
+              Accept
+            </Anchor>
+            <Anchor>
+              Decline
+            </Anchor>
+          </Menu>
+        :
+          null
+        }
+        </Box>
+      </th>
       </tr>
       </thead>
       <tbody>
     {/*this.state.venues.map((result) => (
       <tr key={id}>
+      <td> <CheckBox id="{result.name}" name="label" onChange={...} checked={this.state.isAllSelected} /> </td>
       <td> {name} </td>
       <td> {promoter} </td>
       <td> {status} </td>
@@ -125,34 +178,32 @@ class GuestListPage extends Component {
       </td>
       </tr>
       ))*/}
-      <tr>
-      <td> name </td>
-      <td> promoter </td>
-      <td> status </td>
-      <td>
-      	<Box justify="center" align="center">
+      {this.state.example.map((sample) => (
+        <tr key={sample.id}>
+        {sample.status === 'Pending' ? 
+              <td>
+                <Box align="center">
+                  <CheckBox id={sample.id} onChange={this.testFunc} checked={this.state.isAllSelected}/>
+                </Box>
+              </td>
+          : 
+              <td> </td>
+        }
+        <td> {sample.name} </td>
+        <td> {sample.promoter} </td>
+        <td> {sample.status} </td>
+        <td>
+          <Box justify="center" align="center">
+          {sample.status === 'Pending' ? 
           <Button label="Approve" icon={<CheckmarkIcon />} onClick={this.testFunc} />
-      	</Box>
-      </td>
-      </tr>
-      <tr>
-      <td> name </td>
-      <td> promoter </td>
-      <td> status </td>
-      <td>
-
-      </td>
-      </tr>
-      <tr>
-      <td> name </td>
-      <td> promoter </td>
-      <td> status </td>
-      <td>
-      	<Box justify="center" align="center">
-          <Button label="Approve" icon={<CheckmarkIcon />} onClick={this.testFunc} />
-      	</Box>
-      </td>
-      </tr>
+            : 
+              <td> </td>
+          }
+          </Box>
+        </td>
+        </tr>
+        )) 
+      }
     </tbody>
     </Table>
     </div>
